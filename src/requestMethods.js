1092c0ea97138b53fa9124dc54e00111665f3502
prathmeshppawar <2020.prathmesh.pawar@ves.ac.in>
2023-12-16 01:31:06 +0530

    project done

--- src/requestMethods.js
+++ src/requestMethods.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const BASE_URL = "http://localhost:4001/api/";
+const BASE_URL = "https://leaderboard-moh6.onrender.com/api/";
 
 export const publicRequest = axios.create({
   baseURL: BASE_URL,
